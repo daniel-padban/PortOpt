3a93a71ebb05658edf58e73851924d80aa9a5201
danibahnani <danielpadban@gmail.com>
2025-02-03 17:15:02 +0100

    changed from log to simple returns

--- backtesting.py
+++ backtesting.py
@@ -1,7 +1,8 @@
-from datetime import date
+from datetime import date, timedelta
 import yfinance as yf
 import pandas as pd
 import numpy as np
+from allocator import WeightOptimizer
 
 
 class BackTester():
@@ -30,7 +31,7 @@ class BackTester():
         return period_return
     
     def cumulative_returns(self,returns:pd.DataFrame):
-        cumulative_returns = (returns+1).cumprod(0)-1
+        cumulative_returns = np.exp(returns.cumsum()) - 1
         return cumulative_returns
     
     def period_sharpe(self,returns:pd.DataFrame,risk_free_annual:float,intervals_per_period=252):
@@ -43,3 +44,39 @@ class BackTester():
         sharpe = (excess_return*intervals_per_period-daily_risk_free)/std
         return sharpe
 
+
+class repeatingBacktester():
+    def __init__(self, 
+                current_comp:set,
+                changes_path:str,
+                eval_start:date, 
+                eval_end:date,
+                lookback:int|timedelta,
+                rebalancing:int|timedelta,
+                alpha:float,
+                beta:float,
+                gamma:float,
+                rf:float,
+                rf_period:timedelta,
+                num_iter:int,
+                weight_decay:float,
+                lr,):
+        
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+        self.num_iter = num_iter
+        self.lr = lr
+        self.rf = rf
+        self.rf_period = rf_period
+        self.weight_decay = weight_decay
+
+        pass
+
+    def _register_changes(self):
+        changes_df = pd.read_csv(self.changes_path)
+        
+
+    def _optimize(self,num_assets,returns):
+        allocator = WeightOptimizer(self.num_iter,self.lr,num_assets,self.rf,self.rf_period,self.weight_decay)
+        allocator.optimize_weights(self.alpha,self.beta,self.gamma,returns)
